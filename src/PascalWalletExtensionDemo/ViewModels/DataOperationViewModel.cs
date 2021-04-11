ae8c5d0b5675d671b94f8c1414a2031a5b1bc9af
Mikus Vanags <mikus.vanags@logicsresearchcentre.com>
2021-04-11 12:36:12 +0300

    Added signer selection feeture, but seems, that blockchain stucks when signer is selected.

--- src/PascalWalletExtensionDemo/ViewModels/DataOperationViewModel.cs
+++ src/PascalWalletExtensionDemo/ViewModels/DataOperationViewModel.cs
@@ -14,29 +14,30 @@ namespace PascalWalletExtensionDemo.ViewModels
     {
         private PascalConnector _connector;
         private InfoMessageViewModel _infoMessage;
-        private uint _senderAccount;
+        private Account _senderAccount;
+        private Account _signerAccount;
         private uint _receiverAccount;
         private decimal _fee;
         private string _identifier;
         private string _message;
-        private List<uint> _accounts;
+        private List<Account> _accounts;
+        private List<Account> _accountsWithPasc;
 
         public DataOperationViewModel(PascalConnector connector)
         {
             _connector = connector;
-            Identifier = Guid.NewGuid().ToString().ToUpper();
             ReceiverAccount = 834853;
+            GenerateGuid();
 
             InfoMessage = new InfoMessageViewModel("Connecting to Pascal Wallet...", null);
 
             //this should be created on the UI thread
             var error = new InfoMessageViewModel("Failed to connect to Pascal Wallet", () => Application.Current.Shutdown(), true);
 
-            _connector.GetWalletAccountsAsync(max: 100000).ContinueWith(task => {
+            _connector.GetWalletAccountsAsync(max: 300).ContinueWith(task => {
                 if(task.Result.Result != null)
                 {
-                    var accounts = task.Result.Result.Select(n => n.AccountNumber).OrderBy(n => n);
-                    Accounts = new List<uint>(accounts);
+                    Accounts = task.Result.Result.OrderBy(n => n.AccountNumber).ToList();
                     InfoMessage = null;
                 }
                 else
@@ -45,12 +46,13 @@ namespace PascalWalletExtensionDemo.ViewModels
                 }
             }).ConfigureAwait(false);
 
-            SendCommand = new RelayCommandAsync(SendDataOperation);
+            SendCommand = new RelayCommandAsync(SendDataOperation, parameter => CanSend());
+            GenerateGuidCommand = new RelayCommand(GenerateGuid);
         }
 
         public async Task SendDataOperation()
         {
-            var sendingDataResponse = await _connector.SendDataAsync(SenderAccount, ReceiverAccount, Identifier, null, DataType.ChatMessage, 0, 0, Fee, Message);
+            var sendingDataResponse = await _connector.SendDataAsync(SenderAccount.AccountNumber, ReceiverAccount, Identifier, SignerAccount?.AccountNumber, DataType.ChatMessage, 0, 0, Fee, Message);
             if (sendingDataResponse.Result != null)
             {
                 InfoMessage = new InfoMessageViewModel($"DataOperation sent successfully.", () => InfoMessage = null);
@@ -74,7 +76,17 @@ namespace PascalWalletExtensionDemo.ViewModels
             }
         }
 
-        public List<uint> Accounts
+        public List<Account> AccountsWithPasc
+        {
+            get { return _accountsWithPasc; }
+            set
+            {
+                _accountsWithPasc = value;
+                OnPropertyChanged(nameof(AccountsWithPasc));
+            }
+        }
+
+        public List<Account> Accounts
         {
             get { return _accounts; }
             set
@@ -84,13 +96,32 @@ namespace PascalWalletExtensionDemo.ViewModels
             }
         }
 
-        public uint SenderAccount
+        public Account SenderAccount
         {
             get { return _senderAccount; }
             set
             {
+                if(value != null)
+                {
+                    AccountsWithPasc = Accounts.Where(n => n.EncodedPublicKey == value.EncodedPublicKey).ToList();
+                }
+                else
+                {
+                    AccountsWithPasc = new List<Account>();
+                }
+
                 _senderAccount = value;
                 OnPropertyChanged(nameof(SenderAccount));
+            }
+        }
+
+        public Account SignerAccount
+        {
+            get { return _signerAccount; }
+            set
+            {
+                _signerAccount = value;
+                OnPropertyChanged(nameof(SignerAccount));
             }
         }
 
@@ -135,5 +166,16 @@ namespace PascalWalletExtensionDemo.ViewModels
         }
 
         public ICommand SendCommand { get; private set; }
+        public ICommand GenerateGuidCommand { get; private set; }
+
+        private void GenerateGuid()
+        {
+            Identifier = Guid.NewGuid().ToString().ToUpper();
+        }
+
+        private bool CanSend()
+        {
+            return SenderAccount != null;
+        }
     }
 }
